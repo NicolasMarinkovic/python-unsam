e01c74fc35f69d1a66d1c7f58127c2903fccb7ae
Nicolas Marinkovic <nicolasnahuel01@gmail.com>
2022-08-05 17:56:33 -0300

    Finish inclusive.py

--- Clase01/inclusive.py
+++ Clase01/inclusive.py
@@ -2,12 +2,14 @@
 # Consinga:
 # completá el siguiente código para reemplazar todas las letras 'o' que figuren en el último o anteúltimo caracter de cada palabra por una 'e'. 
 
-frase = 'todos somos programadores'
+frase =  'todos somos programadores'
 palabras = frase.split()
+i = 0
 for palabra in palabras:
-    if palabra[-1] == 's' and palabra[-2] == 'o':
-        palabra.replace('o','e')
-        print("Hola")
-    frase_t = ' '.join(palabras)
-    print(frase_t)
-'todes somes programadores'
+    if palabra[-2:] == 'os': #busco si la palabra termina en os
+        newWord = palabra[:-2] + 'es'   #agrego a una nueva variable el resto de la palabra + 'es'. Porque no puedo modificar el valor de un string
+        del palabras[i]
+        palabras.insert(i, newWord)
+    i += 1
+
+print(' '.join(palabras))
